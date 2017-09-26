c13a154452f73c058c1ae40e9c96f156d9110eca
tarashub <tarasbabyla@gmail.com>
2017-09-26 12:21:22 +0300

    Working release

--- src/main/java/shoes/com/ua/ShoeseApplication.java
+++ src/main/java/shoes/com/ua/ShoeseApplication.java
@@ -2,6 +2,11 @@ package shoes.com.ua;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
 
 @SpringBootApplication
 public class ShoeseApplication {
@@ -9,5 +14,20 @@ public class ShoeseApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ShoeseApplication.class, args);
 
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+		templateResolver.setTemplateMode("XHTML");
+		templateResolver.setPrefix("templates/");
+		templateResolver.setSuffix(".html");
+
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.setTemplateResolver(templateResolver);
+
+		ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
+		viewResolver.setTemplateEngine(engine);
+		return viewResolver;
 	}
 }
