5518de4a5cf8cb9e77b66b26cc896037c8f12312
tarashub <tarasbabyla@gmail.com>
2017-09-29 12:07:50 +0300

    Working release

---
+++ src/main/java/shoes/com/ua/config/MvcConfig.java
@@ -0,0 +1,20 @@
+package shoes.com.ua.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class MvcConfig extends WebMvcConfigurerAdapter {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/register").setViewName("register");
+        registry.addViewController("/sales").setViewName("sales");
+        registry.addViewController("/new").setViewName("new");
+    }
+}
